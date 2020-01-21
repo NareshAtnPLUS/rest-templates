4bcb9bdb0ede6c778eb438914e042b980e1dfd87
Naresh Kumar <nareshkumarAtnPLUS@gmail.com>
2020-01-21 06:19:46 +0530

    Registration form incomplete

--- angular-src/src/app/components/account/register/register.component.ts
+++ angular-src/src/app/components/account/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent   {
+  profileForm = this.fb.group({
+    firstName:['',Validators.required],
+    lastName:['',Validators.required],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      zip: ['']
+    }),
+  })
+  constructor(private fb: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  
 
 }
